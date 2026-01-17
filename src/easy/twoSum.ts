ad091ca1668d110328824237be40f0519423833f
Daniel Ivanov <ivanoffworkh@gmail.com>
2026-01-17 17:31:32 +0200

    feat: optimal solution

--- src/easy/twoSum.ts
+++ src/easy/twoSum.ts
@@ -11,3 +11,22 @@ function twoSum(nums: number[], target: number): number[] {
     }
   }
 }
+
+// O(n)
+function twoSumOptimal(nums: number[], target: number): number[] {
+  let valuesMap = new Map<number, number>();
+  let indexes: number[] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const value = nums[i];
+
+    if (valuesMap.has(target - value)) {
+      indexes[0] = valuesMap.get(target - value);
+      indexes[1] = i;
+      break;
+    }
+    valuesMap.set(value, i);
+  }
+
+  return indexes;
+}
